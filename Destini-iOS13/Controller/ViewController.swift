bbc7c445c80e2f8482dd78e7e81edd5f5b791b57
Michel Kim <kim98.michel@gmail.com>
2023-06-27 23:11:07 -0300

    Create a Structure

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var leftButton: UIButton!
     @IBOutlet weak var rightButton: UIButton!
     
+    let stories = [
+        Story(title: "You see a fork in the road", choice1: "Take a left.", choice2: "Take a right."),
+        Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
+        Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
+    ]
+    
+    var storyPosition = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        storyTextLabel.text = "You see a fork in thre road"
-        leftButton.setTitle("Take a left.", for: .normal)
-        rightButton.setTitle("Take a right.", for: .normal)
+        storyTextLabel.text = stories[storyPosition].title
+        leftButton.setTitle(stories[storyPosition].choice1, for: .normal)
+        rightButton.setTitle(stories[storyPosition].choice2, for: .normal)
     }
 
 
